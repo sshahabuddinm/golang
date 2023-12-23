848726686d6f5a5b895ee44b664b45baec6f1268
sshahabuddinm <110201419+sshahabuddinm@users.noreply.github.com>
2023-12-23 22:46:20 +0000

    test: added the test for format capabilities

--- pkg/vault/main_test.go
+++ pkg/vault/main_test.go
@@ -24,3 +24,23 @@ func TestCreatePolicyString(t *testing.T) {
 		t.Errorf("CreatePolicyString() returned unexpected result.\nExpected:\n%s\nGot:\n%s", expected, result)
 	}
 }
+
+func TestFormatCapabilities(t *testing.T) {
+	// Test case 1: Non-empty capabilities
+	capabilities1 := []string{"read", "write", "delete"}
+	result1 := formatCapabilities(capabilities1)
+	expected1 := `["read", "write", "delete"]`
+
+	if result1 != expected1 {
+		t.Errorf("formatCapabilities() returned unexpected result.\nExpected:\n%s\nGot:\n%s", expected1, result1)
+	}
+
+	// Test case 2: Empty capabilities
+	capabilities2 := []string{}
+	result2 := formatCapabilities(capabilities2)
+	expected2 := "[]"
+
+	if result2 != expected2 {
+		t.Errorf("formatCapabilities() returned unexpected result.\nExpected:\n%s\nGot:\n%s", expected2, result2)
+	}
+}
