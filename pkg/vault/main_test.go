50606ba65f00a66ef1f7fa007c1d847895a67221
sshahabuddinm <110201419+sshahabuddinm@users.noreply.github.com>
2023-12-23 22:57:49 +0000

    test: added test for create new vault client

--- pkg/vault/main_test.go
+++ pkg/vault/main_test.go
@@ -44,3 +44,78 @@ func TestFormatCapabilities(t *testing.T) {
 		t.Errorf("formatCapabilities() returned unexpected result.\nExpected:\n%s\nGot:\n%s", expected2, result2)
 	}
 }
+
+// func TestCreateNewVaultClient(t *testing.T) {
+// 	// Test case 1: Valid host and token
+// 	host1 := "http://localhost:8200"
+// 	token1 := "myroot"
+
+// 	client1, err1 := createNewVaultClient(host1, token1)
+// 	if err1 != nil {
+// 		t.Errorf("createNewVaultClient() returned an unexpected error for valid input: %v", err1)
+// 	}
+// 	if client1 == nil {
+// 		t.Error("createNewVaultClient() returned a nil client for valid input")
+// 	}
+
+// 	// Test case 2: Invalid host
+// 	host2 := "invalid-host"
+// 	token2 := "myroot"
+// 	client2, err2 := createNewVaultClient(host2, token2)
+// 	if err2 == nil {
+// 		t.Error("createNewVaultClient() did not return an error for an invalid host")
+// 	}
+// 	if client2 != nil {
+// 		t.Error("createNewVaultClient() returned a non-nil client for an invalid host")
+// 	}
+
+// 	// Test case 3: Invalid token
+// 	host3 := "http://localhost:8200"
+// 	token3 := ""
+
+// 	client3, err3 := createNewVaultClient(host3, token3)
+// 	if err3 == nil {
+// 		t.Error("createNewVaultClient() did not return an error for an empty token")
+// 	}
+// 	if client3 != nil {
+// 		t.Error("createNewVaultClient() returned a non-nil client for an empty token")
+// 	}
+// }
+
+func TestCreateNewVaultClient(t *testing.T) {
+	// Test case 1: Valid host and token
+	host1 := "http://localhost:8200"
+	token1 := "myroot"
+
+	client1, err1 := createNewVaultClient(host1, token1)
+	if err1 != nil {
+		t.Errorf("createNewVaultClient() returned an unexpected error for valid input: %v", err1)
+	}
+	if client1 == nil {
+		t.Error("createNewVaultClient() returned a nil client for valid input")
+	}
+
+	// // Test case 2: Invalid host
+	// host2 := "invalid-host"
+	// token2 := "myroot"
+
+	// client2, err2 := createNewVaultClient(host2, token2)
+	// if err2 == nil {
+	// 	t.Error("createNewVaultClient() should return an error for an invalid host")
+	// }
+	// if client2 != nil {
+	// 	t.Error("createNewVaultClient() should return a nil client for an invalid host")
+	// }
+
+	// // Test case 3: Invalid token
+	// host3 := "http://localhost:8200"
+	// token3 := ""
+
+	// client3, err3 := createNewVaultClient(host3, token3)
+	// if err3 == nil {
+	// 	t.Error("createNewVaultClient() should return an error for an empty token")
+	// }
+	// if client3 != nil {
+	// 	t.Error("createNewVaultClient() should return a nil client for an empty token")
+	// }
+}
